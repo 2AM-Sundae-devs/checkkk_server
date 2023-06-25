5d85fa53c159f0dded9d39c0387a2b0951f8053b
ubermensch0608 <melancholyee28@gmail.com>
2023-06-25 15:31:58 +0900

    update: mogoose configs

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,9 @@
 import express from 'express';
 import cors from 'cors';
 import cookieParser from 'cookie-parser';
+import dotenv from 'dotenv';
+
+dotenv.config();
 
 import { homeRouter } from './routes';
 import { detailRouter } from './routes/api/DetailRouter';
@@ -8,6 +11,7 @@ import { chartRouter } from './routes/api/chartRouter';
 import { loginRouter } from './routes/api/LoginRouter';
 import { apiRouter } from './routes/api/api';
 import signupRouter from './routes/api/SignupRouter';
+import mongoose from 'mongoose';
 
 const app = express();
 
@@ -25,6 +29,20 @@ app.use('/charts', chartRouter);
 app.use('/details', detailRouter);
 app.use('/signUp', signupRouter);
 app.use('/login', loginRouter);
+
+const MONGO_URL = process.env.MONGO_URI || '';
+
+mongoose
+  .connect(MONGO_URL, {
+    autoIndex: true,
+  })
+  .then(() => {
+    console.log('MongoDB has been connected');
+  })
+  .catch((err) => {
+    console.error(err);
+    console.log('Error!');
+  });
 
 app.listen(app.get('PORT'), () => {
   console.log(`Server is running on PORT:${app.get('PORT')}`);
