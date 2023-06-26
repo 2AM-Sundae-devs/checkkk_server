74c64edb3ae7050b1263e0663b7bbb9a1e796efc
ubermensch0608 <melancholyee28@gmail.com>
2023-06-26 12:09:00 +0900

    update: password bcrypt

--- src/routes/api/LoginRouter.ts
+++ src/routes/api/LoginRouter.ts
@@ -1,6 +1,8 @@
 import express from 'express';
 import bcrypt from 'bcrypt';
+
 import { User } from '../../models/User';
+import { setResponse } from '../../@types/response';
 
 const router = express.Router();
 
@@ -10,19 +12,37 @@ export const loginRouter = router.post('/', async (req, res) => {
 
     const user = await User.findOne({ nickname });
     if (!user) {
-      return res.status(400).json({ message: 'User does not exist' });
+      return res
+        .status(400)
+        .json(
+          setResponse(
+            'Y',
+            '가입한 유저가 없습니다.\n회원 가입 후 이용해주세요.',
+          ),
+        );
     }
-    const validPassword = await bcrypt.compare(password, user.password);
 
-    if (!validPassword) {
-      return res.status(400).json({ message: 'Invalid password' });
+    const salt = bcrypt.genSaltSync(9);
+    const hashedPassword = bcrypt.hashSync(password, salt);
+    const isPasswordValid = bcrypt.compareSync(password, hashedPassword);
+
+    if (!isPasswordValid) {
+      return res
+        .status(400)
+        .json(
+          setResponse(
+            'Y',
+            '"비밀번호를 잘못 입력했습니다.\n입력하신 내용을 다시 확인해주세요"',
+          ),
+        );
     }
 
-    // res.cookie('userId', user._id.toString(), { httpOnly: true });
-
-    res.status(200).json({ message: 'Login successful' });
+    res.cookie('userId', user._id.toString(), { httpOnly: true });
+    res.status(200).json(setResponse('N', '로그인 성공!'));
   } catch (err) {
-    console.log('login err : ', err);
-    res.status(500).json({ message: 'something went wrong' });
+    console.error(err, 'login err');
+    res
+      .status(500)
+      .json(setResponse('Y', '서버 예외처리 에러입니다. 서버에 문의해주세요!'));
   }
 });
--- src/routes/api/SignupRouter.ts
+++ src/routes/api/SignupRouter.ts
@@ -1,6 +1,7 @@
 import express from 'express';
 import bcrypt from 'bcrypt';
 import { User } from '../../models/User';
+import { setResponse } from '../../@types/response';
 
 const router = express.Router();
 
@@ -9,38 +10,42 @@ router.post('/', async (req, res) => {
     const { nickname, password } = req.body;
 
     if (!nickname) {
-      return res.status(400).json({ message: 'nickname is required' });
+      return res.status(400).json(setResponse('Y', '닉네임을 입력해주세요.'));
     }
     if (!password) {
-      return res.status(400).json({ message: 'Password is required' });
+      return res.status(400).json(setResponse('Y', '비밀번호를 입력해주세요.'));
     }
 
     const existingUser = await User.findOne({ nickname });
 
     if (existingUser) {
-      return res.status(409).json({ message: 'Email already exists' });
+      return res
+        .status(409)
+        .json(
+          setResponse(
+            'Y',
+            '동일한 닉네임으로 가입한 유저가 있습니다.\n다른 닉네임을 사용해주세요.',
+          ),
+        );
     }
 
-    // const salt = await bcrypt.genSalt();
-    // const hashedPassword = await bcrypt.hash(password, salt);
-
-    // const newUser = new User({
-    //   nickname,
-    //   password: hashedPassword,
-    // });
+    const salt = bcrypt.genSaltSync(9);
+    const hashedPassword = bcrypt.hashSync(password, salt);
 
     const newUser = new User({
       nickname,
-      password,
+      password: hashedPassword,
     });
     await newUser.save();
 
     res.cookie('userId', newUser._id.toString(), { httpOnly: true });
 
-    res.status(201).json({ message: 'login success' });
+    res.status(201).json(setResponse('N', '회원가입 완료!'));
   } catch (err) {
-    console.log('signup err : ', err);
-    res.status(500).json({ message: 'something went wrong' });
+    console.log(err, 'at signUp');
+    res
+      .status(500)
+      .json(setResponse('Y', '서버 예외처리 에러입니다. 서버에 문의해주세요!'));
   }
 });
 
